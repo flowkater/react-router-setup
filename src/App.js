ca1113bdbd1a7a6ecf974424c61b13d57599c352
flowkater <chjw102@gmail.com>
2016-10-11 17:45:20 +0900

    neted router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { Router, Route, Link, hashHistory } from 'react-router';
+
+const Home = (props) => <div><h1>Home</h1><Links />{props.children}</div>;
+const About = (props) => <div><h1>About</h1><Links />{props.children}</div>;
+const Contact = () => <div><h1>Contact</h1><Links /></div>;
+
+const Links = () =>
+  <nav>
+    <Link activeClassName="active" to="/">Home</Link>
+    <Link activeClassName="active" to="/about">About</Link>
+    <Link activeClassName="active" to="/about/contact">Contact</Link>
+  </nav>
 
 export class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <Router history={ hashHistory }>
+        <Route path="/" component={Home}>
+          <Route path="about" component={About}>
+            <Route path="contact" component={Contact}></Route>
+          </Route>
+        </Route>
+      </Router>
     );
   }
 }
